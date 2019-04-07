492a2ded332d3c0783ba6084c0d91f600112a996
KobayashiKz <anuuihsa9711212@gmail.com>
2019-04-07 22:51:07 +0900

    Add music photo area

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:music_app/bottom_controls.dart';
+import 'package:music_app/songs.dart';
 import 'package:music_app/theme.dart';
 
 void main() => runApp(MyApp());
@@ -54,6 +58,13 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Container(
                     width: 125.0,
                     height: 125.0,
+                    child: ClipOval(
+                      clipper: CircleClipper(),
+                      child: Image.network(
+                        demoPlaylist.songs[0].albumArtUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -65,96 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
 
               /// Song title, artist name, and controls
-
-              Container(
-                color: accentColor,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 40.0, bottom: 50.0),
-                  child: Column(
-                    children: <Widget>[
-                      RichText(
-                        text: TextSpan(
-                            text: "",
-                            children: [
-                              TextSpan(
-                                  text: "Song Title\n",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 4.0,
-                                    height: 1.5,
-                                  )
-                              ),
-                              TextSpan(
-                                  text: "Artist Name",
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.75),
-                                    fontSize: 12.0,
-                                    letterSpacing: 3.0,
-                                    height: 1.5,
-                                  )
-                              )
-                            ]
-                        ),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(top: 40.0),
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(child: Container(),),
-                            IconButton(
-                              icon: Icon(
-                                Icons.skip_previous,
-                                color: Colors.white,
-                                size: 35.0,
-                              ),
-                              onPressed: () {
-                                // TODO
-                              },
-                            ),
-
-                            Expanded(child: Container(),),
-                            RawMaterialButton(
-                              shape: CircleBorder(),
-                              fillColor: Colors.white,
-                              splashColor: lightAccentColor,
-                              highlightColor: lightAccentColor.withOpacity(0.5),
-                              elevation: 10.0,
-                              highlightElevation: 5.0,
-                              onPressed: () {
-                                // TODO:
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Icon(
-                                  Icons.play_arrow,
-                                  color: darkAccentColor,
-                                    size: 35.0,
-                                ),
-                              ),
-                            ),
-
-                            Expanded(child: Container(),),
-                            IconButton(
-                              icon: Icon(
-                                Icons.skip_next,
-                                color: Colors.white,
-                                size: 35.0,
-                              ),
-                              onPressed: () {
-                                // TODO
-                              },),
-
-                            Expanded(child: Container(),),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              )
+              new BottomControls()
             ],
           ),
       ),
